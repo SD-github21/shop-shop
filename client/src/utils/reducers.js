99fb0fe147877946dbfbacfd7012377c4e1f6cd3
SD-github21 <sapandonde.gmail.com>
2022-08-08 00:29:27 -0500

    added code to implement reducers and  global state

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,46 @@
+import { useReducer } from 'react';
+
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from "./actions";
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        // If action type value is the value of "UPDATE_PRODUCTS", return a new state object
+        // with an updated products array
+        case UPDATE_PRODUCTS:
+            return {
+                ...state,
+                products: [...action.products],
+            };
+
+        // If action type value is the value of "UPDATE_CATEGORIES", return a new state object
+        // with an updated categories array
+
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories],
+            };
+
+
+        // If action value type is the value of "UPDATE_CURRENT_CATEGORY", return a new state object
+        // with an updated categories array
+
+        case UPDATE_CURRENT_CATEGORY:
+            return {
+                ...state,
+                currentCategory: action.currentCategory
+            }
+
+        // If it's none of these actions, do not update state at all and keep things the same!
+        default:
+            return state;
+    }
+};
+
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState)
+}
