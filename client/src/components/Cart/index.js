372565b7795a96d11d91dfec370674fb4b49ceda
SD-github21 <sapandonde.gmail.com>
2022-08-10 00:28:35 -0500

    added shopping cart functionality

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,10 +8,19 @@ import { TOGGLE_CART } from '../../utils/actions';
 
 const Cart = () => {
 
-    const [state, dispatch] = useStoreContext();
 
+    const [state, dispatch] = useStoreContext();
+    
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
+    }
+
+    function calculateTotal() {
+        let sum = 0;
+        state.cart.forEach(item => {
+            sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2);
     }
 
     if (!state.cartOpen) {
@@ -23,17 +32,19 @@ const Cart = () => {
           </div>
         );
       }
+
       
     return (
         <div className="cart">
             <div className="close" onClick={toggleCart}>[close]</div>
             <h2>Shopping Cart</h2>
+            { state.cart.length ? (
             <div>
-                <CartItem item={{ name: 'Camera', image: 'camera.jpg', price:5, purchaseQuantity:3 }} />
-                <CartItem item={{ name: 'Soap', image:'soap.jpg', price:6, purchaseQuantity:4 }} />
-
+                { state.cart.map(item => (
+                <CartItem key= {item._id} item={item} />
+                ))}
                 <div className="flex-row space-between">
-                    <strong>Total: $0</strong>
+                    <strong>Total: ${calculateTotal()}</strong>
                     {
                         Auth.loggedIn() ?
                             <button>
@@ -44,6 +55,14 @@ const Cart = () => {
                     }
                 </div>
             </div>
+            ) : (
+             <h3>
+                <span role="img" aria-label="shocked">
+                    😱
+                </span>
+                You haven't added anything to your cart yet!
+             </h3>
+            )}
         </div>
     );
 };
